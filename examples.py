5ec9f82f3a9be4300618747433e94c8bbe19c48f
jsiegelman1 <jacob.siegelman1@gmail.com>
2016-07-24 15:59:03 -0700

    added more tests/whoops

--- examples.py
+++ examples.py
@@ -1,6 +1,9 @@
 def filter2(list, cond):
+	ret = []
 	for i in iter(list):
-		if not cond(list):
-			return False
-	return True	
-assert filter2([1, -1, 0, 5, 2], lambda x: x > 0)
+		if cond(i):
+			ret.append(i)
+	return ret
+	
+assert filter2([1, -1, 0, 5, 2], lambda x: x > 0) == [1, 5, 2]
+assert filter2(["Hello", "Hi", "Bye", "This is a string"], lambda x: len(x) > 3) == ["Hello", "This is a string"]
