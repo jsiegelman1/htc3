34ca1d199bcc65a0d42a64795068504ed4edf67b
jsiegelman1 <jacob.siegelman1@gmail.com>
2016-07-24 16:26:44 -0700

    (8.4) added a dumb sum example since build-list and foldr don't really exist in pythong

--- examples.py
+++ examples.py
@@ -9,3 +9,8 @@ def filter2(list, cond):
 assert filter2([1, -1, 0, 5, 2], lambda x: x > 0) == [1, 5, 2]
 assert filter2(["Hello", "Hi", "Bye", "This is a string"], lambda x: len(x) > 3) == ["Hello", "This is a string"]
 assert map(len, ["Hello", "Hi", "Bye", "This is a string"]) == [5, 2, 3, 16]
+
+def sum_to(n):
+	return sum(range(n))
+assert sum_to(3) == (1 + 2)
+assert sum_to(10) == (1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9)
